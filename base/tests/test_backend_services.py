f19231efb571bb8227dbc2463c244c13f00b0c04
kevin lugaziva <kevin.lugaziwa@financeplan.biz>
2019-08-28 15:48:57 +0300

    HM-25 | add test for base backend services

--- base/tests/test_backend_services.py
+++ base/tests/test_backend_services.py
@@ -8,37 +8,37 @@ pytestmark = pytest.mark.django_db
 
 class TestStateService(object):
     """
-	Test the State Model Services
-	"""
+    Test the State Model Services
+    """
 
     def test_get(self):
         """
-		Test State get service
-		"""
+        Test  State get service
+        """
         mixer.blend('base.State', name="Active")
         state = StateService().get(name="Active")
         assert state is not None, 'Should have an State object'
 
     def test_filter(self):
         """
-		Test State filter service
-		"""
+        Test State filter service
+        """
         mixer.cycle(3).blend('base.State')
         states = StateService().filter()
         assert len(states) == 3, 'Should have 3 State objects'
 
     def test_create(self):
         """
-		Test State create service
-		"""
+        Test State create service
+        """
         state = StateService().create(name = "Active")
         assert state is not None, 'Should have a State object'
         assert state.name == "Active", "Created State name is equals to Active"
 
     def test_update(self):
         """
-		Test State update service
-		"""
+        Test State update service
+        """
         state = mixer.blend('base.State')
         state = StateService().update(state.id, name = "Active")
         assert state.name == "Active", 'Should have the same name'
