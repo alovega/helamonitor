3087f03f517e5cf535581ad04fa9e4a85384e401
kevin lugaziva <kevin.lugaziwa@financeplan.biz>
2019-09-02 09:50:05 +0300

    HM-30 | add a process for analysing created system status

--- api/backend/system_status.py
+++ api/backend/system_status.py
@@ -1,7 +1,7 @@
 
 import logging
 
-from core.backend.services import SystemMonitorService
+from core.backend.services import SystemMonitorService, EventService
 
 lgr = logging.getLogger(__name__)
 
@@ -16,18 +16,29 @@ class SystemStatusLogger(object):
 
 	system_status = None
 
-	def create_system_status(self, **kwargs):
+	def save_system_status(self, **kwargs):
 		if self.system_status is None:
 			try:
 				self.system_status = SystemMonitorService().create(**kwargs) # creates a system_status object
+				self.analyse_system_status(system_status = self.system_status)
 				return self.system_status
 			except Exception as e:
-				lgr.exception("Monitor Log exception: %s" % e)
+				lgr.exception("Status Log exception: %s" % e)
 
-	def analyse_system_status(self, system_status):
-		if system_status.status != 200:
-			system = system_status.system
-			endpoint = system_status.endpoint
+	@staticmethod
+	def analyse_system_status(system_status, desired_time=20, **kwargs):
+		if system_status.status_code != 200:
+			try:
+				event = EventService().create(**kwargs)
+				return event
+			except Exception as e:
+				lgr.exception("Event Log exception: %s" %e)
+		elif system_status.response_time > desired_time:
+			try:
+				event = EventService().create(**kwargs)
+				return event
+			except Exception as e:
+				lgr.exception("Event Log exception: %s" %e)
 
 	def generate_status_report(self):
 		pass
