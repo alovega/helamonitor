dbbaeaec6511e459cf306e3359388b2aac3631de
kevin lugaziva <kevin.lugaziwa@financeplan.biz>
2019-09-02 10:24:59 +0300

    HM-31 | comment the code for future reference

--- api/backend/system_status.py
+++ api/backend/system_status.py
@@ -17,16 +17,29 @@ class SystemStatusLogger(object):
 	system_status = None
 
 	def save_system_status(self, **kwargs):
+		"""
+		This method  creates  system status for monitoring purposes
+		:param kwargs:
+		:return: system_status
+		"""
 		if self.system_status is None:
 			try:
-				self.system_status = SystemMonitorService().create(**kwargs) # creates a system_status object
-				self.analyse_system_status(system_status = self.system_status)
+				self.system_status = SystemMonitorService().create(**kwargs)  # creates a system_status object
+				self.analyse_system_status(system_status = self.system_status)  # analyse whether to create an event
+				# based on the logged status
 				return self.system_status
 			except Exception as e:
 				lgr.exception("Status Log exception: %s" % e)
 
 	@staticmethod
 	def analyse_system_status(system_status, desired_time=20, **kwargs):
+		"""
+		This method analyses the created system_status is it okay, down or in a failed state
+		:param system_status:
+		:param desired_time: a set time that a response_time must meet for it's request to be considered okay
+		:param kwargs:
+		:return: event
+		"""
 		if system_status.status_code != 200:
 			try:
 				event = EventService().create(**kwargs)
