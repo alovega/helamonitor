22f93722d2bada6c63c7c5a867237086bbcdcebb
kevin lugaziva <kevin.lugaziwa@financeplan.biz>
2019-09-03 14:31:12 +0300

    HM-31 | refactor my health monitor processor

--- api/backend/system_status.py
+++ api/backend/system_status.py
@@ -1,19 +1,16 @@
 
 import logging
 
-from core.backend.services import SystemMonitorService, EventService
+from core.backend.services import SystemMonitorService, EventService, InterfaceService, SystemService, EndpointService
+from base.backend.services import StateService, EventTypeService, EscalationLevelService
 
 lgr = logging.getLogger(__name__)
 
 
-class SystemStatusLogger(object):
+class MonitorProcessor(object):
 	"""
 	class for logging status  of  system micro-services
 	"""
-
-	def __init__(self, **kwargs):
-		pass
-
 	system_status = None
 
 	def save_system_status(self, **kwargs):
@@ -23,35 +20,82 @@ class SystemStatusLogger(object):
 		:return: system_status
 		"""
 		if self.system_status is None:
-			try:
-				self.system_status = SystemMonitorService().create(**kwargs)  # creates a system_status object
-				self.analyse_system_status(system_status = self.system_status)  # analyse whether to create an event
-				# based on the logged status
-				return self.system_status
-			except Exception as e:
-				lgr.exception("Status Log exception: %s" % e)
+			if kwargs['code'] == 200:
+				system = SystemService().get(name=kwargs['system'])
+				status = StateService().get(name='operational')
+				endpoint = EndpointService().get(endpoint=kwargs['url'])
+				data1 = {
+					"system": system,
+					"status_code": kwargs['response']['code'],
+					"code": kwargs['code'],
+					"version": system.version,
+					"response_time": kwargs['response_time'],
+					"endpoint": endpoint,
+					"state": status
+				}
+				try:
+					self.system_status = SystemMonitorService().create(**data1)  # creates a system_status object
+					# event = self.analyse_system_status(
+					# 	system_status = self.system_status, desired_time = self.system_status.endpoint.default_time,
+					# 	**data1
+					# )  # analyse whether to create an event based on the logged status and response time
+					return self.system_status #event
+				except Exception as e:
+					lgr.exception("Status Log exception: %s" % e)
+
+			else:
+				system = SystemService().get(name = kwargs['system'])
+				interface = InterfaceService().get(system=kwargs['system']),
+				event_type = EventTypeService.get(name='Error'),
+				escalation_level = EscalationLevelService.get(name='High'),
+				status = StateService().get(name='not operational')
+				data1 = {
+					"system": system,
+					"version": system.version,
+					"response_time": None,
+					"description": kwargs['message'],
+					"interface": interface,
+					"method": kwargs['message'],
+					"response": kwargs['response'].json(),
+					"request": kwargs['response'].request.method,
+					"event_type": event_type,
+					"state": status,
+					"escalation_level": escalation_level,
+					"status_code": kwargs['response']['code'],
+				}
+
+				try:
+					self.system_status = SystemMonitorService().create(**data1)  # creates a system_status object
+					event = EventTypeService().create(**data1) # creates an Event object
+					return self.system_status, event
+				except Exception as e:
+					lgr.exception("Status Log exception: %s" % e)
 
 	@staticmethod
-	def analyse_system_status(system_status, desired_time=20, **kwargs):
+	def analyse_system_status(system_status, desired_time, **kwargs):
 		"""
-		This method analyses the created system_status is it okay, down or in a failed state
+		This method analyses the created system_status response time is within the desired time
 		:param system_status:
 		:param desired_time: a set time that a response_time must meet for it's request to be considered okay
-		:param kwargs:
+		:param kwargs: this is a dictionary of data to be used by EventService() object
 		:return: event
 		"""
-		if system_status.status_code != 200:
+		if system_status.response_time > desired_time:
+			description = "%s is okay but with a slow response time" % system_status.endpoint
+			event_type = EventTypeService.get(name='Debug')
+			escalation_level = EscalationLevelService.get(name = 'Low')
+			method = None
+			response = kwargs['response'].json()
+			request = kwargs['response'].request.method
+			interface = InterfaceService().get(system = kwargs['system']),
+
 			try:
-				event = EventService().create(**kwargs)
+				event = EventService().create(
+					description, event_type, escalation_level, method, request, response, interface,  **kwargs
+				)
 				return event
 			except Exception as e:
-				lgr.exception("Event Log exception: %s" %e)
-		elif system_status.response_time > desired_time:
-			try:
-				event = EventService().create(**kwargs)
-				return event
-			except Exception as e:
-				lgr.exception("Event Log exception: %s" %e)
+				lgr.exception("Event Log exception: %s" % e)
 
 	def generate_status_report(self):
 		pass
--- api/tests/test_system_status.py
+++ api/tests/test_system_status.py
@@ -1,11 +1,13 @@
 import pytest
 from mixer.backend.django import mixer
-from api.backend.system_status import SystemStatusLogger
+import requests
+from api.backend.system_status import MonitorProcessor
+from core.backend.services import EndpointService
 
 pytestmark = pytest.mark.django_db
 
 
-class TestSystemStatusLogger(object):
+class TestMonitorProcessor(object):
 	"""
 	class for testing system_status Logger
 	"""
@@ -15,18 +17,30 @@ class TestSystemStatusLogger(object):
 		:return: a system object
 		"""
 		system = mixer.blend('core.System')
-		state = mixer.blend('base.State')
+		state = mixer.blend('base.State', name='operational')
 		response_time = 300
-		endpoint = mixer.blend('core.Endpoint')
-
-		system_monitor_fields = {
+		endpoint = mixer.blend('core.Endpoint', endpoint="http://127.0.0.1/blah/blah")
+		kwargs = {
+			'response': {u'code': u'800.200.001', u'data': {u'reference': u'A0Z9Z9Z90'}},
+			'url': 'http://127.0.0.1/blah/blah',
 			'system': system,
 			'state': state,
-			'endpoint': endpoint,
-			'response_time': response_time
+			'response_time': 0.212627,
+			'version': system.version,
+			'status_code': '200',
+			'code': 200
 		}
 
-		monitor_manager = SystemStatusLogger().create_system_status(**system_monitor_fields)
+		# system_monitor_fields = {
+		# 	'status_code': 200,
+		# 	'system': system,
+		# 	'state': state,
+		# 	'endpoint': endpoint,
+		# 	'response_time': response_time
+		# }
+
+		monitor_manager = MonitorProcessor().save_system_status( **kwargs)
+		print kwargs['system']
 		print monitor_manager
 		assert monitor_manager is not None, "Should create a system status"
 		assert monitor_manager.system == system
