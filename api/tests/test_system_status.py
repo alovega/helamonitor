08528793cb8dfed4ec4ca93dab8bbf15b695fa3c
kevin lugaziva <kevin.lugaziwa@financeplan.biz>
2019-09-04 15:41:16 +0300

    HM-31 | refactor my test to pass

--- api/tests/test_system_status.py
+++ api/tests/test_system_status.py
@@ -1,3 +1,5 @@
+import datetime
+
 import pytest
 from mixer.backend.django import mixer
 from api.backend.system_status import MonitorProcessor
@@ -18,11 +20,14 @@ class TestMonitorProcessor(object):
 		interface = mixer.blend('core.Interface', system=system)
 		state = mixer.blend('base.State', name='UP')
 		response_time = 300
-		endpoint = mixer.blend('core.Endpoint', endpoint="http://127.0.0.1/blah/blah", system=system)
+		endpoint_type = mixer.blend('base.EndpointType', is_queried = True)
+		endpoint = mixer.blend(
+			'core.Endpoint', endpoint="http://127.0.0.1/blah/blah", system=system, endpoint_type=endpoint_type
+		)
 		kwargs = {
 			'response': {u'code': u'800.200.001', u'data': {u'reference': u'A0Z9Z9Z90'}},
 			'url': 'http://127.0.0.1/blah/blah',
-			'response_time': 0.212627,
+			'response_time': datetime.timedelta(seconds = 0.212627),
 			'status_code': 200,
 		}
 
@@ -39,7 +44,10 @@ class TestMonitorProcessor(object):
 		system = mixer.blend('core.System')
 		interface = mixer.blend('core.Interface', system = system)
 		state = mixer.blend('base.State', name = 'Down')
-		endpoint = mixer.blend('core.Endpoint', endpoint = "http://127.0.0.1/blah/blah", system = system)
+		endpoint_type = mixer.blend('base.EndpointType', is_queried =True)
+		endpoint = mixer.blend(
+			'core.Endpoint', endpoint = "http://127.0.0.1/blah/blah", system = system, endpoint_type= endpoint_type
+		)
 		escalation_level = mixer.blend('base.EscalationLevel', name='High')
 		event_type = mixer.blend('base.EventType', name='Critical')
 		kwargs = {
