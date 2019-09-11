44935bf51eecb80659861f385ed7d4314c4e54fb
Victor Nguli <victornjoseph@gmail.com>
2019-09-11 16:48:34 +0300

    HM-42 | update incident logger

--- api/backend/processors/incident_logger.py
+++ api/backend/processors/incident_logger.py
@@ -6,6 +6,7 @@ import logging
 
 from core.backend.services import SystemService, IncidentService, IncidentEventService
 from base.backend.services import IncidentTypeService, StateService, EventTypeService, EscalationLevelService
+
 lgr = logging.getLogger(__name__)
 
 
@@ -13,66 +14,68 @@ class IncidentLogger(object):
 	"""
 	Class for logging incidents
 	"""
+
 	@staticmethod
-	def create_incident(incident_type, system, escalation_level, name='', description='', event_type='',
-							escalated_events=None, priority_level='', **kwargs):
+	def create_incident(
+			incident_type, system, escalation_level, name = '', description = '', event_type = None,
+			escalated_events = None, priority_level = '', message = '', **kwargs):
 		"""
-		Creates an incident from the escalated events
+		Creates a realtime incident based on escalated events or scheduled incident based on user reports
 		@param incident_type: Type of the incident to be created
 		@type incident_type: str
-		@param system: System where the incident is to be reported
+		@param system: The system which the incident will be associated with
 		@type system: str
 		@param name: Title of the incident
 		@type name: str
 		@param description: Details on the incident
 		@type description: str
-		@param event_type: Type of the event that triggered the creation of the incident
-		@type event_type: str
-		@param escalated_events: One or more events that satisfied an escalation rule.
-		@type escalated_events: list
+		@param event_type: Type of the event(s) that triggered creation of the incident, if its event driven.
+		@type event_type: str | None
+		@param escalated_events: One or more events in the escalation if the incident is event driven.
+		@type escalated_events: list | None
 		@param priority_level: The priority level to be assigned to the incident.
-		@type priority_level: str
-		@param escalation_level: Configuration for the recipients of a notification after incident creation
+		@type priority_level: str | None
+		@param escalation_level: Level at which an escalation is configured with a set of recipients
 		@type escalation_level: str
-		@param kwargs:
-		@return:
+		@param kwargs: Extra key-value arguments to pass for incident logging
+		@return: Status of the incident creation in a response code dictionary
+		@rtype: dict
 		"""
 		try:
-			system = SystemService().get(name=system, state__name = "Active")
-			incident_type = IncidentTypeService().get(name=incident_type, state__name="Active")
-			escalation_level = EscalationLevelService().get(name=escalation_level, state__name="Active", system=system)
+			system = SystemService().get(name = system, state__name = "Active")
+			incident_type = IncidentTypeService().get(name = incident_type, state__name = "Active")
+			escalation_level = EscalationLevelService().get(
+				name = escalation_level, state__name = "Active", system = system)
 			if system is None or incident_type is None or escalation_level is None:
 				return {"code": "800.400.002"}
 
 			if incident_type.name == "Realtime" and escalated_events is not None and event_type is not None:
-				incident = IncidentService().filter(event_type__name=event_type).exclude(
-					state__name='Resolved').order_by('-date_created').first()
+				incident = IncidentService().filter(event_type__name = event_type).exclude(
+					state__name = 'Resolved').order_by('-date_created').first()
 				if incident:
 					incident.priority_level += 1
+					return {'code': '800.200.001'}
 				else:
-					incident = IncidentService().create(
-						name = name, description = description, state = StateService().get(name = "Active"),
-						incident_type = incident_type, system = system, event_type=EventTypeService().get(
-							name='event_type'),
-						priority_level = EventTypeService().get(name = event_type).priority_level()
-					)
-				for event in escalated_events:
-					IncidentEventService().create(
-						event=event, incident=incident, state=StateService().get(name = "Active")
-					)
+					priority_level = EventTypeService().get(name = event_type).priority_level()
 			else:
-				incident = IncidentService().create(
-					name = name, description = description, state = StateService().get(name = "Active"),
-					incident_type = incident_type, system = system, event_type=EventTypeService().get(
-							name='event_type'),
-					priority_level = int(priority_level)
-				)
+				priority_level = int(priority_level)
 
+			incident = IncidentService().create(
+				name = name, description = description, state = StateService().get(name = "Active"),
+				incident_type = incident_type, system = system, event_type = EventTypeService().get(
+					name = 'event_type'),
+				priority_level = priority_level
+			)
 			if incident is not None:
+				if escalated_events is not None:
+					for event in escalated_events:
+						IncidentEventService().create(
+							event = event, incident = incident, state = StateService().get(name = "Active")
+						)
 				notification = IncidentLogger.send_notification(
-					incident, escalation_level = escalation_level, message="message")
+					incident, escalation_level = escalation_level, message = "message")
 				if notification.get('code') != '800.200.001':
-					lgr.warning("Notification sending failed")
+					lgr.error("Notification sending failed")
 				return {'code': '800.200.001'}
 		except Exception as ex:
 			lgr.exception("Incident Logger exception %s" % ex)
