fb9317a9d83bb517703c9527ce479487bcf9aa6e
kevin lugaziva <kevin.lugaziwa@financeplan.biz>
2019-09-11 09:07:39 +0300

    HM-31 | update health_monitor test

--- api/tests/test_health_monitor.py
+++ api/tests/test_health_monitor.py
@@ -30,56 +30,6 @@ class TestMonitorInterface(object):
 
 		monitor_manager = MonitorInterface.perform_health_check()
 		print (monitor_manager)
-		assert monitor_manager is  None, "Should log systems statuses %s " % monitor_manager
+		assert monitor_manager is not None, "Should log systems statuses %s " % monitor_manager
+		assert monitor_manager.get('code') == "800.200.001", "Should return a general success code"
 
-	# def test_create_event_if_status_fail(self):
-	# 	"""
-	# 	test create system state and event if it is a failure
-	# 	:return: a system object
-	# 	"""
-	# 	system = mixer.blend('core.System')
-	# 	interface = mixer.blend('core.Interface', system = system)
-	# 	state = mixer.blend('base.State', name = 'Down')
-	# 	endpoint_type = mixer.blend('base.EndpointType', is_queried =True)
-	# 	endpoint = mixer.blend(
-	# 		'core.Endpoint', endpoint = "http://127.0.0.1/blah/blah", system = system, endpoint_type= endpoint_type
-	# 	)
-	# 	escalation_level = mixer.blend('base.EscalationLevel', name='High')
-	# 	event_type = mixer.blend('base.EventType', name='Critical')
-	# 	kwargs = {
-	# 		'response': {u'code': u'800.200.001', u'data': {u'reference': u'A0Z9Z9Z90'}},
-	# 		'url': 'http://127.0.0.1/blah/blah',
-	# 		'response_time': 0.212627,
-	# 		'status_code': 400,
-	# 		'code': '800.200.001'
-	# 	}
-	#
-	# 	monitor_manager = MonitorProcessor().save_system_status(**kwargs)
-	# 	print (monitor_manager)
-	# 	assert monitor_manager is not None, "Should create a system status"
-	#
-	# def test_create_event_if_slow_response_time(self):
-	# 	"""
-	# 	test create system state and event if it is a failure
-	# 	:return: a system object
-	# 	"""
-	# 	system = mixer.blend('core.System')
-	# 	interface = mixer.blend('core.Interface', system = system)
-	# 	state = mixer.blend('base.State', name = 'UP')
-	# 	endpoint_type = mixer.blend('base.EndpointType', is_queried =True)
-	# 	endpoint = mixer.blend(
-	# 		'core.Endpoint', endpoint = "http://127.0.0.1/blah/blah", system = system, endpoint_type= endpoint_type,
-	# 		optimal_response_time = datetime.timedelta(milliseconds = 5)
-	# 	)
-	# 	event_type = mixer.blend('base.EventType', name='Debug')
-	# 	kwargs = {
-	# 		'response': {u'code': u'800.200.001', u'data': {u'reference': u'A0Z9Z9Z90'}},
-	# 		'url': 'http://127.0.0.1/blah/blah',
-	# 		'response_time': datetime.timedelta(seconds = 0.212627),
-	# 		'status_code': 200,
-	# 		'code': '800.200.001'
-	# 	}
-	#
-	# 	monitor_manager = MonitorProcessor().save_system_status(**kwargs)
-	# 	print (monitor_manager)
-	# 	assert monitor_manager is not None, "Should create a system status"
