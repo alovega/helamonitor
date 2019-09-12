2eab5daaebf1f8d1f77325d83232d69f0c89b969
kevin lugaziva <kevin.lugaziwa@financeplan.biz>
2019-09-12 12:57:20 +0300

    HM-48 | create the notification interface

--- api/backend/interfaces/notification_interface.py
+++ api/backend/interfaces/notification_interface.py
@@ -4,8 +4,8 @@ Class for creating new incidents and logging incident updates
 """
 import logging
 
-from core.backend.services import SystemRecipientService, NotificationService
-from base.backend.services import StateService, NotificationTypeService
+from core.backend.services import NotificationService
+from base.backend.services import StateService
 
 import datetime
 
@@ -15,16 +15,15 @@ lgr = logging.getLogger(__name__)
 class NotificationLogger(object):
 
 	@staticmethod
-	def send_notification(message, message_type, recipients):
+	def send_notification(message, message_type, recipient):
 		try:
 			data = NotificationService().create(
-					message = notification.get('message'),
-					notification_type = notification.get('notification_type'),
-					recipient = notification.get('recipient'), incident = notification.get('incident'),
-					system = notification.get('system'), state = notification.get('state'),
+					message = message,
+					notification_type = message_type,
+					recipient = recipient,
+					state = StateService().get('Active')
 				)
-
-				if data:
+			if data:
 					message1 = {
 						"destination": data.recipient, "message_type": data.notification_type,
 						"lang": None,
@@ -35,7 +34,14 @@ class NotificationLogger(object):
 							'time': datetime.datetime.now().time().strftime('%I:%M%p')
 						}
 					}
-
+					# to do a call to notification API check if returns a code for success
+					if message:
+						NotificationService().update(id=data.id, state=StateService().get(name='Sent'))
+						return {"code": "800.200.001"}
+					else:
+						NotificationService().update(id = data.id, state = StateService().get(name = 'Failed'))
+						return {"code": "200.400.006"}
+			return {"code": "200.400.005"}
 		except Exception as e:
 			lgr.exception("Notification logger exception %s" % e)
 
