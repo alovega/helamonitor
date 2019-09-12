bf74698c3d483af2a605e0d3056d6ec516599bee
Victor Nguli <victornjoseph@gmail.com>
2019-09-12 21:36:31 +0300

    HM-50 | add email list generation for notifications

--- api/backend/interfaces/incident_administration.py
+++ api/backend/interfaces/incident_administration.py
@@ -4,9 +4,10 @@ Class for incident Administration
 """
 import logging
 
-from core.backend.services import IncidentService, IncidentLogService, IncidentEventService, SystemService
+from core.backend.services import IncidentService, IncidentLogService, IncidentEventService, SystemService, \
+	SystemRecipientService, RecipientService
 from base.backend.services import LogTypeService, StateService, EscalationLevelService, EventTypeService, \
-	IncidentTypeService
+	IncidentTypeService, NotificationTypeService
 from django.contrib.auth.models import User
 
 lgr = logging.getLogger(__name__)
@@ -83,7 +84,8 @@ class IncidentAdministrator(object):
 						)
 						if not incident_event:
 							lgr.error("Error creating incident-events")
-				# TODO add send_notification call
+				system_recipients = SystemRecipientService().filter(
+					escalation_level = escalation_level, system = system)
 				return {'code': '800.200.001'}
 		except Exception as ex:
 			lgr.exception("Incident Logger exception %s" % ex)
@@ -127,7 +129,14 @@ class IncidentAdministrator(object):
 				log_type = log_type, priority_level = priority_level, state = StateService().get(name = state)
 			)
 			if incident_log:
-				# TODO Add a send_notification call
+				system_recipients = SystemRecipientService().filter(
+					escalation_level = escalation_level, system = incident.system).values('recipient')
+				recipients = RecipientService().filter(id__in = system_recipients)
+				email_list = [recipient["email"] for recipient in recipients.values("email")]
+				phone_numbers = [recipient["phone_number"] for recipient in recipients.values("phone_number")]
+				notification = NotificationLogger().send_notification(
+					message = incident.description, recipients = email_list, message_type = "Email"
+				)
 				return {'code': '800.200.001'}
 		except Exception as ex:
 			lgr.exception("Incident Administration exception %s" % ex)
--- api/tests/test_event_log.py
+++ api/tests/test_event_log.py
@@ -2,7 +2,7 @@
 """
 Tests for event_creation process
 """
-
+from datetime import timedelta
 import pytest
 from mixer.backend.django import mixer
 from api.backend.interfaces.event_log import EventLog
@@ -36,16 +36,31 @@ class TestEventLog(object):
         system = mixer.blend('core.System', state=state)
         interface = mixer.blend('core.Interface', system=system, state=state)
         event_type = mixer.blend('base.EventType', state=state)
-        # incident_type = mixer.blend("base.IncidentType", state=state, name="realtime")
-        event = mixer.blend(
+        escalation_level = mixer.blend("base.EscalationLevel", state=state, name="Critical")
+        log_type = mixer.blend("base.LogType", state=state, name="PriorityUpdate")
+        incident_type = mixer.blend(
+            "base.IncidentType", state=state, name="Realtime")
+        recipient1 = mixer.blend("core.Recipient", state=state)
+        recipient2 = mixer.blend("core.Recipient", state=state)
+        system_recipient1 = mixer.blend(
+            "core.SystemRecipient", system=system, escalation_level=escalation_level, recipient=recipient1
+        )
+        system_recipient2 = mixer.blend(
+            "core.SystemRecipient", system=system, escalation_level=escalation_level, recipient=recipient2
+        )
+        event1 = mixer.blend(
             'core.Event', event_type=event_type, system=system, interface=interface, state=state,
             description='Test Event description', code='12345'
         )
-        # escalation_rule = mixer.blend(
-        #     "core.EscalationRule", system=system, event_type=event_type, nth_event=1,
-        #     duration = timedelta(seconds=5),
-        #     state = state
-        # )
+        event = mixer.blend(
+            'core.Event', event_type=event_type, system=system, interface=interface, state=state,
+            description='description', code='123'
+        )
+        escalation_rule = mixer.blend(
+            "core.EscalationRule", system=system, event_type=event_type, nth_event=1,
+            duration = timedelta(seconds=5), state = state, escalation_level=escalation_level
+        )
 
+        event_escalation1 = EventLog().escalate_event(event)
         event_escalation = EventLog().escalate_event(event)
-        assert event_escalation.get('code') == '800.200.001', "Should escalate event successfully"
+        assert event_escalation is None, "Should escalate event successfully"
