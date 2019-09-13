daf8ba1dd08e23583ed32cf36e182078c719a1bf
Victor Nguli <victornjoseph@gmail.com>
2019-09-13 15:05:48 +0300

    HM-54 | add create_incident view and url

--- api/views.py
+++ api/views.py
@@ -5,7 +5,7 @@ import logging
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 
-from api.backend.interfaces.event_log import EventLog
+from api.backend.interfaces.event_log import EventLog, IncidentAdministrator
 from base.backend.utilities import get_request_data
 
 lgr = logging.getLogger(__name__)
@@ -23,10 +23,34 @@ def report_event(request):
 	try:
 		data = get_request_data(request)
 		event = EventLog().log_event(
-			data.get('event_type'), data.get('system'), data.get('interface', None), data.get('response', None),
-			data.get('request', None), data.get('code', None), data.get('description', None)
+			event_type = data.get('event_type'), system = data.get('system'), interface = data.get('interface'),
+			response = data.get('response'), request = data.get('request'), code = data.get('code'),
+			description = data.get('description')
 		)
 		return JsonResponse(event)
 	except Exception as ex:
-		lgr.exception("Event logging exception" % ex)
+		lgr.exception("Event logging Exception: %s" % ex)
+	return JsonResponse({'code': '800.500.001'})
+
+
+@csrf_exempt
+def create_incident(request):
+	"""
+	Creates incidents from users
+	@param request: The Django WSGI Request to process
+	@type request: WSGIRequest
+	@return: A response code to indicate successful incident creation or otherwise
+	@rtype: dict
+	"""
+	try:
+		data = get_request_data(request)
+		incident = IncidentAdministrator().log_incident(
+			incident_type = data.get('incident_type'), system = data.get('system'), name = data.get('name'),
+			escalation_level = data.get('escalation_level'), description = data.get('description'),
+			priority_level = data.get('priority_level'), event_type = data.get('event_type', None),
+			state = data.get('state', 'Investigating'), escalated_events = data.get('escalated_events', None)
+		)
+		return JsonResponse(incident)
+	except Exception as ex:
+		lgr.exception('Incident creation Exception: %s' % ex)
 	return JsonResponse({'code': '800.500.001'})
