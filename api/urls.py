0b008fc949e156afa4fb2eecd5fcb5011875af24
Victor Nguli <victornjoseph@gmail.com>
2019-09-13 16:00:02 +0300

    HM-57 | add incident update view

--- api/urls.py
+++ api/urls.py
@@ -4,9 +4,10 @@ The URLs for API endpoints
 """
 from django.conf.urls import url
 
-from api.views import report_event, create_incident
+from api.views import report_event, create_incident, update_incident
 
 urlpatterns = [
     url(r'^report_event/$', report_event, name = 'report_event'),
     url(r'^create_incident/$', create_incident, name = 'create_incident'),
+    url(r'^update_incident/$', update_incident, name = 'update_incident'),
 ]
