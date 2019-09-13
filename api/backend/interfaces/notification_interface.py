1b407d1e78030ffee2cc8dd83e17613fbf760a10
kevin lugaziva <kevin.lugaziwa@financeplan.biz>
2019-09-13 09:23:05 +0300

    HM-48 | update notification interface

--- api/backend/interfaces/notification_interface.py
+++ api/backend/interfaces/notification_interface.py
@@ -5,7 +5,7 @@ Class for creating new incidents and logging incident updates
 import logging
 
 from core.backend.services import NotificationService, SystemRecipientService
-from base.backend.services import StateService
+from base.backend.services import StateService, NotificationTypeService
 
 import datetime
 
@@ -15,47 +15,50 @@ lgr = logging.getLogger(__name__)
 class NotificationLogger(object):
 
 	@staticmethod
-	def send_notification(message, message_type, recipient):
+	def send_notification(message, message_type, recipients):
 		"""
 		Create and sends a notification
 		@param message: a string of the content to be sent
 		@type:str
 		@param message_type: a notification_type object
-		@type: Object
-		@param recipient: a user object
-		@type:Object
+		@type: str
+		@param recipients: a list of recipients
+		@type:list
 		@return: returns a dict of a code for success or failure
 		@rtype: dict
 		"""
 		try:
-			data = NotificationService().create(
+			for recipient in recipients:
+				data = NotificationService().create(
 					message = message,
-					notification_type = message_type,
+					notification_type = NotificationTypeService().get(name = message_type),
 					recipient = recipient,
 					state = StateService().get(name = 'Active')
 				)
-			if data:
-				system = SystemRecipientService().get(recipient=data.recipient, state__name='Active')
-				message = {
-					"destination": data.recipient, "message_type": data.notification_type.name,
-					"lang": None,
-					"corporate_id": system.system.id, "message_code": 'HPS0006',
-					"replace_tags": {
-						"code": None, 'corporate':system.system.name,
-						'date': datetime.date.today().strftime('%d/%m/%y'),
-						'time': datetime.datetime.now().time().strftime('%I:%M%p')
+				if data:
+					if str(message_type) == 'Email':
+						system = SystemRecipientService().get(recipient__email = data.recipient, state__name = 'Active')
+					else:
+						system = SystemRecipientService().get(
+							recipient__phone_number = data.recipient.phone_number, state__name = 'Active'
+						)
+					message = {
+						"destination": data.recipient, "message_type": data.notification_type.name,
+						"lang": None,
+						"corporate_id": system.system.id, "message_code": 'HPS0006',
+						"replace_tags": {
+							"code": None, 'corporate': system.system.name,
+							'date': datetime.date.today().strftime('%d/%m/%y'),
+							'time': datetime.datetime.now().time().strftime('%I:%M%p')
 						}
 					}
-				# to do a call to notification API check if it returns a code for success
-				if message:
-					NotificationService().update(data.id, state= StateService().get(name='Sent'))
-					return {"code": "800.200.001"}
-				else:
-					NotificationService().update(data.id, state = StateService().get(name = 'Failed'))
-					return {"code": "200.400.006"}
-			return {"code": "200.400.005"}
+					# to do a call to notification API check if it returns a code for success
+					if message:
+						print NotificationService().update(data.id, state = StateService().get(name = 'Sent'))
+					else:
+						print NotificationService().update(data.id, state = StateService().get(name = 'Failed'))
+						lgr.warn("Message sending failed %s" % message)
+				return {"code": "200.400.005"}
+			return {"code": "800.200.001"}
 		except Exception as e:
 			lgr.exception("Notification logger exception %s" % e)
-
-
-
--- api/tests/test_notification_interface.py
+++ api/tests/test_notification_interface.py
@@ -2,7 +2,7 @@ import pytest
 from mixer.backend.django import mixer
 
 from api.backend.interfaces.notification_interface import NotificationLogger
-from core.backend.services import SystemRecipientService
+from core.backend.services import SystemRecipientService, RecipientService
 
 pytestmark = pytest.mark.django_db
 
@@ -18,15 +18,25 @@ class TestIncidentLogger(object):
 		state = mixer.blend('base.State', name = 'Active')
 		system = mixer.blend('core.System', state = state)
 		escalation_level = mixer.blend('base.EscalationLevel')
-		recipient = mixer.blend('core.Recipient', first_name = 'Kevin', state = state)
+		recipient1 = mixer.blend('core.Recipient', first_name = 'Kevin', phone_number= +254776054478, state = state)
+		recipient2 = mixer.blend(
+			'core.Recipient', first_name = 'Elly', phone_number = +2541136575757, state = state
+		)
+
 		system_recipient = SystemRecipientService().create(
-			recipient = recipient, system = system, state = state, escalation_level = escalation_level
+			recipient = RecipientService().get(phone_number=recipient1.phone_number), system = system, state = state,
+			escalation_level = escalation_level
+		)
+		system_recipient = SystemRecipientService().create(
+			recipient = RecipientService().get(phone_number = recipient2.phone_number), system = system, state = state,
+			escalation_level = escalation_level
 		)
 		message_type = mixer.blend('base.NotificationType', name = 'SMS')
+		print message_type.name
 		message = 'Hey Listen to BBC news today'
 
 		notification = NotificationLogger().send_notification(
-			recipient = recipient, message = message, message_type = message_type,
+			recipients = [recipient1, recipient2], message = message, message_type = message_type.name,
 		)
 
-		assert notification == {'code': '800.200.001'}, "Should create an incident %s " % notification
+		assert notification == {'code': '000.200.001'}, "Should create an incident %s " % notification
