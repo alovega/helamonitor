5f4b867db904c4c55d7e4edb33446017f4536282
kevin lugaziva <kevin.lugaziwa@financeplan.biz>
2019-09-16 08:48:51 +0300

    HM-55 | create health check view and url

--- api/urls.py
+++ api/urls.py
@@ -4,10 +4,11 @@ The URLs for API endpoints
 """
 from django.conf.urls import url
 
-from api.views import report_event, create_incident, update_incident
+from api.views import report_event, create_incident, update_incident, health_check
 
 urlpatterns = [
     url(r'^report_event/$', report_event, name = 'report_event'),
     url(r'^create_incident/$', create_incident, name = 'create_incident'),
     url(r'^update_incident/$', update_incident, name = 'update_incident'),
+    url(r'^health_check/$', health_check, name = 'health_check')
 ]
