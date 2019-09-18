7e3472cd30f21233399e71cc595c12358c82837b
CynthiaSimiyu <cynthia.simiyu@financeplan.biz>
2019-09-18 10:23:30 +0300

    Create Jenkins Test Settings File

---
+++ helamonitor/jenkins_test_settings.py
@@ -0,0 +1,22 @@
+# coding=utf-8
+"""
+The Jenkins test settings for Hela Monitor project
+"""
+from .settings import *
+
+DEBUG = True
+
+# Database
+# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+
+DATABASES = {
+	'default': {
+		'ENGINE': 'django.db.backends.sqlite3',
+		'NAME': os.path.join(BASE_DIR, '../db.sqlite3'),
+	}
+}
+
+EMAIL_BACKEND = 'django.core.mail.backends.locmem.EmailBackend'
+LOGGING = {}
+FILE_CONTENT_ROOT = BASE_DIR
+STATIC_ROOT = BASE_DIR
