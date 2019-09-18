413a745a825100f6688ee0bc4f4c08a9703b1975
Victor Nguli <victornjoseph@gmail.com>
2019-09-18 16:44:10 +0300

    HM-66 | add oauthadmin

--- api/admin.py
+++ api/admin.py
@@ -3,4 +3,15 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-# Register your models here.
+from api.models import Oauth
+
+
+@admin.register(Oauth)
+class OauthAdmin(admin.ModelAdmin):
+	"""
+	Admin for oauth model
+	"""
+	list_filter = ('-date_created',)
+	list_display = ('token', 'expires_at', 'state', 'date_modified', 'date_created')
+	ordering = ('-date_created',)
+	search_fields = ('app_user__app__name', )
