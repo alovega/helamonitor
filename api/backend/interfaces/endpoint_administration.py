7338ffd6e3b804d205a35ecbf593e6ae22d79d0f
kevin lugaziva <kevin.lugaziwa@financeplan.biz>
2019-10-15 12:47:29 +0300

    HM-101 | add update method  in endpoint administration interface

--- api/backend/interfaces/endpoint_administration.py
+++ api/backend/interfaces/endpoint_administration.py
@@ -28,7 +28,7 @@ class EndpointAdministrator(object):
 		@type endpoint_type_id: int
 		@param state_id: the id of  initial state of the created endpoint will have
 		@type state_id: int
-		@return: Response code dictionary to indicate if the incident was created or not
+		@return: Response code dictionary to indicate if the endpoint was created or not
 		@rtype:dict
 		"""
 		try:
@@ -38,9 +38,10 @@ class EndpointAdministrator(object):
 
 			if not(system and endpoint_type and state and name and description and response_time and endpoint):
 				return {"code": "800.400.002"}
-			exist = EndpointService().filter(system = system, endpoint=endpoint)
+			exist = True if EndpointService().filter(system = system, endpoint=endpoint) \
+				else EndpointService().filter(system=system, name=name)
 			if exist:
-				return {"code": "200.400.007", "message": "An endpoint with this url exists"}
+				return {"code": "200.400.007", "message": "An endpoint with this url or name exists"}
 			endpoint = EndpointService().create(
 				name=name, description=description, endpoint=endpoint, system = system, endpoint_type=endpoint_type,
 				optimal_response_time=datetime.timedelta(seconds=response_time), state=state
@@ -51,7 +52,36 @@ class EndpointAdministrator(object):
 		return {"code": "200.400.007"}
 
 	@staticmethod
-	def update_endpoint():
-		pass
+	def update_endpoint(endpoint_id, state_id, description, endpoint, response_time, name):
+		"""
+		@param endpoint_id: id of the endpoint to be edited
+		@type endpoint_id: char
+		@param name: name of endpoint to be created
+		@type name: str
+		@param description: description of endpoint to be created
+		@type description: str
+		@param endpoint:url of endpoint to be created
+		@type endpoint: str
+		@param response_time:average response time the endpoint should take
+		@type response_time: int
+		@param state_id:the id of  initial state of the created endpoint will have
+		@type state_id: int
+		@return: Response code dictionary to indicate if the endpoint was updated or not
+		@rtype: dict
+		"""
 
+		try:
+			state = StateService().get(id=state_id)
+			update_endpoint = EndpointService().get(id = endpoint_id)
+			if not(state and name and description and response_time and endpoint and update_endpoint):
+				return {"code": "800.400.002"}
 
+			endpoint = EndpointService().update(
+				pk= update_endpoint.id, description= description, state= state,
+				optimal_response_time=datetime.timedelta(seconds=response_time), endpoint=endpoint
+			)
+			return {"code": "800.200.001", "message": "successfully updated endpoint: %s" % endpoint.name}
+
+		except Exception as ex:
+			lgr.exception("Endpoint Administration exception %s" % ex)
+		return {"code": "200.400.007"}
--- api/tests/test_endpoint_administration.py
+++ api/tests/test_endpoint_administration.py
@@ -8,14 +8,15 @@ pytestmark = pytest.mark.django_db
 
 class TestEndpointAdministration(object):
 	"""
-	class for testing endpoint administration
-	"""
+    class for testing endpoint administration
+    """
+
 	def test_create_endpoint(self):
 		state = mixer.blend('base.State', name = 'Active')
-		endpoint_type = mixer.blend('base.EndpointType', state=state, name = 'health_endpoint')
-		system = mixer.blend('core.System', state=state)
+		endpoint_type = mixer.blend('base.EndpointType', state = state, name = 'health_endpoint')
+		system = mixer.blend('core.System', state = state)
 		endpoint = EndpointAdministrator.create_endpoint(
-			state_id=state.id, endpoint_type_id=endpoint_type.id, system_id = system.id,
+			state_id = state.id, endpoint_type_id = endpoint_type.id, system_id = system.id,
 			endpoint = "https://mail.google.com", name = "mail", description = "Endpoint for Google mail",
 			response_time = 5
 		)
@@ -24,6 +25,32 @@ class TestEndpointAdministration(object):
 			endpoint = "https://mail.google.com", name = "mail", description = "Endpoint for Google mail",
 			response_time = 5
 		)
+		endpoint3 = EndpointAdministrator.create_endpoint(
+			state_id = state.id, endpoint_type_id = endpoint_type.id, system_id = system.id,
+			endpoint = "https://mail.google.com", name = "", description = "Endpoint for Google mail",
+			response_time = 5
+		)
 		assert endpoint.get('code') == '800.200.001', "Should create an endpoint"
 		assert endpoint2.get('code') == '200.400.007', "Should return error code"
-		assert endpoint2.get('message') == 'An endpoint with this url exists'
+		assert endpoint2.get('message') == 'An endpoint with this url or name exists'
+		assert endpoint3.get('code') == '800.400.002', "Should return error code for missing parameters"
+
+	def test_update_endpoint(self):
+		state = mixer.blend('base.State', name = 'Active')
+		endpoint_type = mixer.blend('base.EndpointType', state = state, name = 'health_endpoint')
+		system = mixer.blend('core.System', state = state)
+		endpoint = mixer.blend(
+			'core.Endpoint', state = state, endpoint_type = endpoint_type, system = system,
+			endpoint = "https://mail.google.com", name = "mail", description = "Endpoint for Google mail",
+			response_time = 5
+		)
+		updated_endpoint = EndpointAdministrator.update_endpoint(
+			endpoint.id,  state.id, description = "Google mail", response_time = 3,
+			endpoint = "https://mail.googl.com", name = "mail"
+		)
+		updated_endpoint2 = EndpointAdministrator.update_endpoint(
+			endpoint.id, state.id, description = "Google mail", response_time = 3,
+			endpoint = "https://mail.googl.com", name = ""
+		)
+		assert updated_endpoint.get('code') == '800.200.001', "Should update an endpoint"
+		assert updated_endpoint2.get('code') == '800.400.002', "Should update an endpoint"
